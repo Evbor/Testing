96b21559b99a2b4f0abe2b62a913829a2d25a5d8
Momo Belia Deviluke <23489366+MomoBeliaDeviluke@users.noreply.github.com>
2018-11-16 02:34:37 -0800

    added README and tensorflow Neural Network test code

---
+++ tf_tutorials/NN_tutorial.py
@@ -0,0 +1,131 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+# Parameters
+training_batch_size = 32
+epochs = 1
+
+# Importing MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Input to NN is a vector and not a 28x28 matrix
+#  therefore we must reshape our data points to be vectors
+x_train = x_train.reshape(-1, 784)
+x_test = x_test.reshape(-1, 784)
+
+# Double checking to make sure the number of data points equals the number of labels
+assert x_train.shape[0] == y_train.shape[0]
+assert x_test.shape[0] == y_test.shape[0]
+
+###########################
+#Building TensorFlow Graph#
+###########################
+
+# Setting up placeholder objects to feed data to
+x_train_placeholder = tf.placeholder(x_train.dtype, x_train.shape)
+y_train_placeholder = tf.placeholder(y_train.dtype, y_train.shape)
+x_test_placeholder = tf.placeholder(x_test.dtype, x_test.shape)
+y_test_placeholder = tf.placeholder(y_test.dtype, y_test.shape)
+
+# Setting up Dataset objects to store data using unfed placeholder objects
+data_x_train = tf.data.Dataset.from_tensor_slices(x_train_placeholder)
+data_y_train = tf.data.Dataset.from_tensor_slices(y_train_placeholder).map(lambda el: tf.one_hot(el, 10))
+data_x_test = tf.data.Dataset.from_tensor_slices(x_test_placeholder)
+data_y_test = tf.data.Dataset.from_tensor_slices(y_test_placeholder).map(lambda el: tf.one_hot(el,10))
+
+# Setting up Training and Testing Dataset objects from previous Dataset objects
+training_dataset = tf.data.Dataset.zip((data_x_train, data_y_train)).shuffle(500).batch(training_batch_size)
+testing_dataset = tf.data.Dataset.zip((data_x_test, data_y_test)).shuffle(500).batch(32)
+
+# Creating a general Iterator that can switch between consuming different datasets through reinitialization
+iterator = tf.data.Iterator.from_structure(training_dataset.output_types, training_dataset.output_shapes)
+
+# Initializing the general Iterator on different datasets
+#  - When traing run the training_init_op command of the graph
+#  - When testing run the testing_init_op command of the graph
+training_init_op = iterator.make_initializer(training_dataset)
+testing_init_op = iterator.make_initializer(testing_dataset)
+
+# Building NN Model
+
+def neural_network(input):
+    # weights connecting layers 1 (input) --> 2
+    Theta_1 = tf.Variable(tf.random_normal([784, 50], stddev = 0.05), name = "Theta_1")
+    Bias_1 = tf.Variable(tf.random_normal([50], stddev = 0.5), name = "Bias_1")
+    # weights connecting layers 2 --> 3
+    Theta_2 = tf.Variable(tf.random_normal([50, 25], stddev = 0.05), name = "Theta_2")
+    Bias_2 = tf.Variable(tf.random_normal([25], stddev = 0.5), name = "Bias_2")
+    # weights connecting layers 3 --> 4 (output)
+    Theta_3 = tf.Variable(tf.random_normal([25, 10], stddev = 0.05), name = "Theta_3")
+    Bias_3 = tf.Variable(tf.random_normal([10]), name = "Bias_3")
+    # Calculating the activation of the layers
+    a2 = tf.add(tf.matmul(input, Theta_1), Bias_1)
+    a2 = tf.nn.relu(a2)
+    a3 = tf.add(tf.matmul(a2, Theta_2), Bias_2)
+    a3 = tf.nn.relu(a3)
+    output = tf.add(tf.matmul(a3, Theta_3), Bias_3)
+    return output
+
+images, labels = iterator.get_next()
+images = tf.cast(images, tf.float32)
+y = neural_network(images)
+
+# Building Cost Function (cross entropy) and training operation
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels = labels, logits = y))
+train = tf.train.AdamOptimizer().minimize(cost)
+
+# Calculating numbers
+prediction = tf.argmax(y, 1)
+equality = tf.equal(prediction, tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
+
+init_op = tf.global_variables_initializer()
+
+########################################
+#Executing The Default TensorFlow Graph#
+########################################
+
+with tf.Session() as sess:
+    sess.run(init_op)
+    
+    #writer = tf.summary.FileWriter("D:\\Programs\\testing\\tf_tutorials\\NN_tutorial_logs")
+    #writer.add_graph(sess.graph)
+    
+    # Training Run
+  
+    for i in range(epochs):
+        sess.run(training_init_op, feed_dict={x_train_placeholder: x_train, y_train_placeholder: y_train})
+        avg_cost = 0
+        avg_acc = 0
+        batch = 1
+        while True:
+            try:
+                C, _, acc = sess.run([cost, train, accuracy])
+                avg_cost = avg_cost + C
+                avg_acc = avg_acc + acc
+                if batch % 10 == 0:
+                    print("Epoch: {}, Batch: {}, Cost: {:.3f}, training accuracy: {:.2f}%".format(i, batch, C, acc * 100))
+                batch = batch + 1
+            except tf.errors.OutOfRangeError:
+                break
+        avg_cost = avg_cost / batch
+        avg_acc = avg_acc / batch 
+        print("Epoch: {}, Average Cost: {:.3f}, average training accuracy: {:.2f}%".format(i, avg_cost, avg_acc * 100))
+        
+    # Testing Run
+    
+    sess.run(testing_init_op, feed_dict={x_test_placeholder: x_test, y_test_placeholder: y_test})
+    batch = 1
+    avg_acc = 0
+    while True:
+        try:
+            acc = sess.run([accuracy])
+            avg_acc = avg_acc + acc[0]
+            batch = batch + 1
+        except tf.errors.OutOfRangeError:
+            break
+    avg_acc = avg_acc / batch
+    print("Average testing set accuracy over {} batches is {:.2f}%".format(batch, avg_acc * 100))
+    
+    
